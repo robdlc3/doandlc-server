9ba43f5b26b729f2c92de9cd51972c9db029889a
Robert De La Cruz <rdlc976@gmail.com>
2023-05-19 14:58:20 -0400

    first commit, new repo

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+    cors({
+        origin: ['http://localhost:3000']  // <== URL of our future React app
+    })
+);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err);
+    });
+
+module.exports = app;
