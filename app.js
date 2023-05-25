024eed76e72040aa138e969dbf7f53112c2bafdc
Robert De La Cruz <rdlc976@gmail.com>
2023-05-25 17:25:24 -0400

    new domain name

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.enable('trust proxy');
 
 app.use(
     cors({
-        origin: ['https://main--boisterous-blini-4ea1ca.netlify.app']  // <== URL of our future React app
+        origin: ['https://doandlc.netlify.app/']  // <== URL of our future React app
     })
 );
 
